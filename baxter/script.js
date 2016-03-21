0c2e9da4b2c4b6ec40eafc3f85c30e8419230d21
Hammer <rhammer@LOUML9606.local>
2016-03-21 00:13:37 -0400

    testing angular.js edits

--- baxter/script.js
+++ baxter/script.js
@@ -4,7 +4,7 @@ angular.module('includeBody', ['ngAnimate'])
   .controller('NowShowing', ['$scope', function($scope) {
     $scope.templates =
       [ { name: 'now-showing.html', url: 'now-showing.html'},
-          name: 'coming-soon.html', url: 'coming-soon.html'];
+        { name: 'coming-soon.html', url: 'coming-soon.html'}];
     $scope.template = $scope.templates[0];
   }]);
 })(window.angular);
