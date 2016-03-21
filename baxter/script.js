cdf8a2eef45a14dd5701d902dc258bbf2993df8a
Hammer <rhammer@LOUML9606.local>
2016-03-21 00:24:59 -0400

    testing angular.js edits

--- baxter/script.js
+++ baxter/script.js
@@ -6,10 +6,10 @@ angular.module('includeBody', ['ngAnimate'])
       [ { name: 'now-showing.html', url: 'now-showing.html'}];
     $scope.template = $scope.templates[0];
   }]);
-
+angular.module('includeBody', ['ngAnimate'])
   .controller('ComingSoon', ['$scope', function($scope) {
     $scope.templates =
       [ { name: 'coming-soon.html', url: 'coming-soon.html'}];
-    $scope.template = $scope.templates[1];
+    $scope.template = $scope.templates[0];
   }]);
 })(window.angular);
