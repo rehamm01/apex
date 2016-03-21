e7db03a9365f5b9f4231262499fb257d1323ff52
Hammer <rhammer@LOUML9606.local>
2016-03-21 00:11:33 -0400

    testing angular.js edits

--- baxter/script.js
+++ baxter/script.js
@@ -1,19 +1,10 @@
-(function(angular1) {
+(function(angular) {
   'use strict';
-angular1.module('includeBody', ['ngAnimate'])
+angular.module('includeBody', ['ngAnimate'])
   .controller('NowShowing', ['$scope', function($scope) {
     $scope.templates =
-      [ { name: 'now-showing.html', url: 'now-showing.html'}];
+      [ { name: 'now-showing.html', url: 'now-showing.html'},
+          name: 'coming-soon.html', url: 'coming-soon.html'];
     $scope.template = $scope.templates[0];
   }]);
-})(window.angular1);
-
-(function(angular2) {
-  'use strict';
-angular2.module('includeBody', ['ngAnimate'])
-  .controller('ComingSoon', ['$scope', function($scope) {
-    $scope.templates =
-      [ { name: 'coming-soon.html', url: 'coming-soon.html'}];
-    $scope.template = $scope.templates[0];
-  }]);
-})(window.angular2);
+})(window.angular);
