7d0f39e37f5186ed3858320086015c62660f605e
Hammer <rhammer@LOUML9606.local>
2016-03-21 01:39:56 -0400

    angular.js edits

--- baxter/script.js
+++ baxter/script.js
@@ -1,14 +1,14 @@
 var moduleA = angular.module("MyModuleA", []);
 moduleA.controller("NowShowing", function($scope) {
     $scope.templates =
-      [ { name: 'template1.html', url: 'template1.html'}];
+      [ { name: 'now-showing.html', url: 'now-showing.html'}];
     $scope.template = $scope.templates[0];
 });
 
 var moduleB = angular.module("MyModuleB", []);
 moduleB.controller("ComingSoon", function($scope) {
     $scope.templates =
-      [ { name: 'template2.html', url: 'template2.html'}];
+      [ { name: 'coming-soon.html', url: 'coming-soon.html'}];
     $scope.template = $scope.templates[0];
 });
 
