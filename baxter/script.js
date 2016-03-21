74137e2ea736d23f82f4cbed2685bd39a9528ac5
Hammer <rhammer@LOUML9606.local>
2016-03-21 00:19:49 -0400

    testing update to angular.js

--- baxter/script.js
+++ baxter/script.js
@@ -3,8 +3,13 @@
 angular.module('includeBody', ['ngAnimate'])
   .controller('NowShowing', ['$scope', function($scope) {
     $scope.templates =
-      [ { name: 'now-showing.html', url: 'now-showing.html'},
-        { name: 'coming-soon.html', url: 'coming-soon.html'}];
+      [ { name: 'now-showing.html', url: 'now-showing.html'}];
     $scope.template = $scope.templates[0];
+  }]);
+
+  .controller('ComingSoon', ['$scope', function($scope) {
+    $scope.templates =
+      [ { name: 'coming-soon.html', url: 'coming-soon.html'}];
+    $scope.template = $scope.templates[1];
   }]);
 })(window.angular);
